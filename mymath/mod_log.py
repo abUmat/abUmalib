0d76783e37418feb79326374fb87cd64d4bd9548
abUmat <cryptid.ojin@gmail.com>
2023-04-23 04:08:54 +0900

    Add mod_log.py

---
+++ mymath/mod_log.py
@@ -0,0 +1,35 @@
+# my module
+from mymath.gcd_lcm import *
+# my module
+def mod_log(a: int, b: int, p: int) -> int:
+    def inv(a: int, p: int) -> int:
+        b = p; x = 1; y = 0
+        while a:
+            q = b // a
+            a, b = b % a, a
+            x, y = y - q * x, x
+        return y + p if y < 0 else y
+    if a % p < 0: a = a % p + p
+    if b % p < 0: b = b % p + p
+    r = 1 % p
+    for f in range(10**8):
+        g = gcd(a, p)
+        if g == 1: break
+        if b % g: return f if r == b else -1
+        b //= g
+        p //= g
+        r = r * (a // g) % p
+    if p == 1: return f
+    ir = inv(r, p)
+    b = b * ir  % p
+    ak = 1
+    baby = {}
+    for k in range(p + 1):
+        if k * k >= p: break
+        if ak not in baby.keys(): baby[ak] = k
+        ak = ak * a % p
+    iak = inv(ak, p)
+    for i in range(k):
+        if b in baby.keys(): return f + i * k + baby[b]
+        b = b * iak % p
+    return -1
