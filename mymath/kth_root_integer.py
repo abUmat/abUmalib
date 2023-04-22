29c14a3d6e5751dd68eff3124e832110d9d700cf
abUmat <cryptid.ojin@gmail.com>
2023-04-23 03:36:08 +0900

    Add kth_root_integer.py

---
+++ mymath/kth_root_integer.py
@@ -0,0 +1,15 @@
+def kth_root_integer(a: int, k: int) -> int:
+    if a <= 1 or k == 1: return a
+    if 64 <= k: return 1
+    def check(n: int) -> bool:
+        x = 1; m = n
+        p = k
+        while p:
+            if p & 1: x *= m
+            p >>= 1
+            m *= m
+        return x <= a
+    n = int(pow(a, 1 / k))
+    while not check(n): n -= 1
+    while check(n + 1): n += 1
+    return n
