733d1b3d86145330eccb1184ff4f35e4f7c09909
abUmat <cryptid.ojin@gmail.com>
2023-04-23 02:03:37 +0900

    Add taylor_shift.py

---
+++ fps/taylor_shift.py
@@ -0,0 +1,17 @@
+# my module
+from fps.formal_power_series import *
+from modulo.binominal import *
+# my module
+def taylor_shift(f: FormalPowerSeries, a: int, C: Binominal):
+    mod = f.mod
+    N = len(f)
+    res = [f[i] * C.fac(i) % mod for i in range(N)]
+    res.reverse()
+    g = [0] * N
+    g[0] = 1
+    for i in range(1, N): g[i] = (g[i - 1] * a % mod) * C.inv(i) % mod
+    res = FormalPowerSeries(res); res.mod = mod
+    res = (res * FormalPowerSeries(g))[:N]
+    res.reverse()
+    for i in range(N): res[i] = res[i] * C.finv(i) % mod
+    return res
