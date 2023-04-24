33a2a7ce9854dbe0848332ebfd1c3db90d9818ec
abUmat <cryptid.ojin@gmail.com>
2023-04-24 22:17:54 +0900

    Update reduce code length

--- misc/fastio.py
+++ misc/fastio.py
@@ -2,42 +2,30 @@ from atexit import register
 from os import read, write
 import sys
 from __pypy__ import builders
-
 class Fastio:
-    def __init__(self):
-        self.ibuf = bytes()
-        self.pil = 0
-        self.pir = 0
-        self.sb = builders.StringBuilder()
+    ibuf = bytes()
+    pil = pir = 0
+    sb = builders.StringBuilder()
     def load(self):
         self.ibuf = self.ibuf[self.pil:]
         self.ibuf += read(0, 131072)
-        self.pil = 0
-        self.pir = len(self.ibuf)
+        self.pil = 0; self.pir = len(self.ibuf)
     def flush(self): write(1, self.sb.build().encode())
     def fastin(self):
         if self.pir - self.pil < 64: self.load()
-        minus = 0
-        x = 0
+        minus = x = 0
         while self.ibuf[self.pil] < 45: self.pil += 1
-        if self.ibuf[self.pil] == 45:
-            minus = 1
-            self.pil += 1
+        if self.ibuf[self.pil] == 45: minus = 1; self.pil += 1
         while self.ibuf[self.pil] >= 48:
             x = x * 10 + (self.ibuf[self.pil] & 15)
             self.pil += 1
         if minus: return -x
         return x
     def fastout(self, x): self.sb.append(str(x))
-    def fastoutln(self, x):
-        self.sb.append(str(x))
-        self.sb.append('\n')
+    def fastoutln(self, x): self.sb.append(str(x)); self.sb.append('\n')
 fastio = Fastio()
-rd = fastio.fastin
-def rdl(n): return [rd() for _ in range(n)]
-wt = fastio.fastout
-wtn = fastio.fastoutln
-def wtnl(l): wtn(' '.join(map(str, l)))
-flush = fastio.flush
+rd = fastio.fastin; wt = fastio.fastout; wtn = fastio.fastoutln; flush = fastio.flush
 register(flush)
-sys.stdin, sys.stdout = None, None
+sys.stdin = None; sys.stdout = None
+def rdl(n): return [rd() for _ in range(n)]
+def wtnl(l): wtn(' '.join(map(str, l)))
