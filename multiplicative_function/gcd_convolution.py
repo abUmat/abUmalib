29ba64aee30042e1109f217462b3419b120bdd46
abUmat <cryptid.ojin@gmail.com>
2023-04-25 00:32:42 +0900

    Add gcd_convolution.py

---
+++ multiplicative_function/gcd_convolution.py
@@ -0,0 +1,10 @@
+# my module
+from ongoing.divisor_multiple_transform import *
+# my module
+def gcd_convolution(a, b, mod):
+    s, t = a[::], b[::]
+    MultipleTransform.zeta_transform_list(s, mod)
+    MultipleTransform.zeta_transform_list(t, mod)
+    for i in range(len(a)): s[i] = s[i] * t[i] % mod
+    MultipleTransform.mobius_transform_list(s, mod)
+    return s
