3051071f9a9af452a6978aac67ed8d1044973947
abUmat <cryptid.ojin@gmail.com>
2023-04-27 00:21:54 +0900

    Update treap

--- rbst/LazyReversibleTreap.py
+++ rbst/LazyReversibleTreap.py
@@ -1,66 +1,82 @@
-from time import time
-start = time()
-from atexit import register
-from os import read, write
-import sys
-sys.setrecursionlimit(10**8)
-from __pypy__ import builders
+from typing import Callable, Tuple, Iterable
+__rngx = 2463534242
+def _rng():
+    global __rngx
+    __rngx ^= __rngx<<13&0xFFFFFFFF
+    __rngx ^= __rngx>>17&0xFFFFFFFF
+    __rngx ^= __rngx<<5&0xFFFFFFFF
+    return __rngx&0xFFFFFFFF
 
-class Fastio:
-    def __init__(self):
-        self.ibuf = bytes()
-        self.pil = 0
-        self.pir = 0
-        self.sb = builders.StringBuilder()
-    def load(self):
-        self.ibuf = self.ibuf[self.pil:]
-        self.ibuf += read(0, 131072)
-        self.pil = 0
-        self.pir = len(self.ibuf)
-    def flush(self): write(1, self.sb.build().encode())
-    def fastin(self):
-        if self.pir - self.pil < 64: self.load()
-        minus = 0
-        x = 0
-        while self.ibuf[self.pil] < 45: self.pil += 1
-        if self.ibuf[self.pil] == 45:
-            minus = 1
-            self.pil += 1
-        while self.ibuf[self.pil] >= 48:
-            x = x * 10 + (self.ibuf[self.pil] & 15)
-            self.pil += 1
-        if minus: return -x
-        return x
-    def fastout(self, x): self.sb.append(str(x))
-    def fastoutln(self, x):
-        self.sb.append(str(x))
-        self.sb.append('\n')
-fastio = Fastio()
-rd = fastio.fastin
-wt = fastio.fastout
-wtn = fastio.fastoutln
-flush = fastio.flush
-register(fastio.flush)
-sys.stdin, sys.stdout = None, None
-
-rngx = 2463534242
-def rng():
-    global rngx
-    rngx ^= rngx<<13&0xFFFFFFFF
-    rngx ^= rngx>>17&0xFFFFFFFF
-    rngx ^= rngx<<5&0xFFFFFFFF
-    return rngx&0xFFFFFFFF
+class LazyReversibleTreapNode:
+    def __init__(self, e, id_):
+        self.l = None
+        self.r = None
+        self.key = e
+        self.sum = e
+        self.lazy = id_
+        self.cnt = 1
+        self.rev = 0
+        self.pr = _rng()
 
 class LazyReversibleTreap:
-    @staticmethod
-    def make_tree(): return None
+    def __init__(self, e: int, id_: int, op: Callable[[int, int], int], mapping: Callable[[int, int], int], composition: Callable[[int, int], int], ts: Callable[[int], int]) -> None:
+        '''
+        e: identity element of op
+        id_: identity mapping s.t. id(x) == x
+        op: S*S -> S
+        mapping: mapping(f, x) := f(x)
+        composition: composition(f, g) := fog
+        '''
+        self.e = e
+        self.id = id_
+        self.op = op
+        self.mapping = mapping
+        self.composition = composition
+        self.ts = ts
 
     @staticmethod
-    def count(t): return t.cnt if t else 0
+    def _count(t: LazyReversibleTreapNode) -> int:
+        return t.cnt if t else 0
 
-    def size(self, t): return self.count(t)
+    def _toggle(self, t: LazyReversibleTreapNode) -> None:
+        if not t: return
+        t.l, t.r = t.r, t.l
+        t.sum = self.ts(t.sum)
+        t.rev ^= 1
 
-    def merge(self, l, r):
+    def _sum(self, t: LazyReversibleTreapNode) -> int:
+        return t.sum if t else self.e
+
+    def _propagate(self, t: LazyReversibleTreapNode, F: int) -> None:
+        if not t: return
+        t.lazy = self.composition(t.lazy, F)
+        t.key = self.mapping(t.key, F)
+        t.sum = self.mapping(t.sum, F)
+
+    def _update(self, t: LazyReversibleTreapNode) -> LazyReversibleTreapNode:
+        if not t: return
+        self._push(t)
+        l, r = t.l, t.r
+        cnt = 1
+        s = t.key
+        if l:
+            cnt += l.cnt
+            s = self.op(l.sum, s)
+        if r:
+            cnt += r.cnt
+            s = self.op(s, r.sum)
+        t.cnt = cnt; t.sum = s
+        return t
+
+    def _push(self, t: LazyReversibleTreapNode) -> None:
+        if t.rev:
+            self._toggle(t.l); self._toggle(t.r)
+            t.rev = 0
+        if t.lazy != self.id:
+            self._propagate(t.l, t.lazy); self._propagate(t.r, t.lazy)
+            t.lazy = self.id
+
+    def _merge(self, l: LazyReversibleTreapNode, r: LazyReversibleTreapNode) -> LazyReversibleTreapNode:
         if not l or not r: return l if l else r
         root = None
         leaf = None
@@ -68,7 +84,7 @@ class LazyReversibleTreap:
         q = []
         while l and r:
             if l.pr >= r.pr:
-                self.push(l)
+                self._push(l)
                 if pos == 0: root = l
                 elif pos == 1: leaf.r = l
                 else: leaf.l = l
@@ -76,56 +92,45 @@ class LazyReversibleTreap:
                 l = l.r
                 pos = 1
             else:
-                self.push(r)
+                self._push(r)
                 if pos == 0: root = r
                 elif pos == 1: leaf.r = r
                 else: leaf.l = r
-                leaf = r                
+                leaf = r
                 r = r.l
                 pos = -1
             q.append(leaf)
         if r: leaf.r = r
         if l: leaf.l = l
-        while q: self.update(q.pop())
-        return self.update(root)
-        
-    def split(self, t, k):
-        if not t: return None, None
-        self.push(t)
-        if k <= self.count(t.l):
-            s1, s2 = self.split(t.l, k)
-            t.l = s2
-            return s1, self.update(t)
-        else:
-            s1, s2 = self.split(t.r, k - self.count(t.l) - 1)
-            t.r = s1
-            return self.update(t), s2
-        
-    def build(self, v):
-        n = len(v)
-        ps = [LazyReversibleTreapNode(e) for e in v]
-        p = [-1] * n
-        st = []
-        for i in range(n):
-            prv = -1
-            while st and ps[i].pr > ps[st[-1]].pr:
-                prv = st.pop()
-            if prv != -1: p[prv] = i
-            if st: p[i] = st[-1]
-            st.append(i)
-        root = -1
-        for i in range(n):
-            if p[i] != -1:
-                if i < p[i]:
-                    ps[p[i]].l = ps[i]
-                else:
-                    ps[p[i]].r = ps[i]
+        while q: self._update(q.pop())
+        return self._update(root)
+
+    def _split(self, t: LazyReversibleTreapNode, k: int) -> Tuple[LazyReversibleTreapNode, LazyReversibleTreapNode]:
+        L, R = [], []
+        while t:
+            self._push(t)
+            if k <= self._count(t.l):
+                R.append(t)
+                t = t.l
             else:
-                root = i
-        self.dfs(ps[root])
-        return ps[root]
-    
-    def dfs(self, t):
+                k -= self._count(t.l) + 1
+                L.append(t)
+                t = t.r
+        l = None
+        while L:
+            tmp = L.pop()
+            tmp.r = l
+            self._update(tmp)
+            l = tmp
+        r = None
+        while R:
+            tmp = R.pop()
+            tmp.l = r
+            self._update(tmp)
+            r = tmp
+        return self._update(l), self._update(r)
+
+    def _dfs(self, t: LazyReversibleTreapNode) -> None:
         from collections import defaultdict
         q = [t]
         route = []
@@ -137,165 +142,83 @@ class LazyReversibleTreap:
             route.append(node)
             if node.l: q.append(node.l)
             if node.r: q.append(node.r)
-        for node in route[::-1]:
-            self.update(node)
+        while route: self._update(route.pop())
 
-    def insert(self, t, k, e):
-        x1, x2 = self.split(t, k)
-        return self.merge(self.merge(x1, LazyReversibleTreapNode(e)), x2)
-    
-    def erase(self, t, k):
-        x1, x2 = self.split(t, k)
-        return self.merge(x1, self.split(x2, 1)[1])
-    
-    @staticmethod
-    def new(e): return LazyReversibleTreapNode(e)
-
-    @staticmethod
-    def toggle(t):
-        if not t: return
-        t.l, t.r = t.r, t.l
-        t.sum = ts(t.sum)
-        t.rev ^= 1
-    
-    @staticmethod
-    def sum(t): return t.sum if t else node_e
-
-    @staticmethod
-    def propagate(t, F):
-        if not t: return
-        t.lazy = composition(t.lazy, F)
-        t.key = mapping(t.key, F)
-        t.sum = mapping(t.sum, F)
-
-    def fold(self, t, a, b):
-        x1, x2 = self.split(t, a)
-        y1, y2 = self.split(x2, b-a)
-        return self.sum(y1), self.merge(x1, self.merge(y1, y2))
-    
-    def reverse(self, t, a, b):
-        x1, x2 = self.split(t, a)
-        y1, y2 = self.split(x2, b-a)
-        self.toggle(y1)
-        return self.merge(x1, self.merge(y1, y2))
-
-    def apply(self, t, a, b, F):
-        x1, x2 = self.split(t, a)
-        y1, y2 = self.split(x2, b-a)
-        self.propagate(y1, F)
-        return self.merge(x1, self.merge(y1, y2))
-    
-    def update(self, t):
-        if not t: return
-        self.push(t)
-        t.cnt = 1
-        t.sum = t.key
-        if t.l:
-            t.cnt += t.l.cnt
-            t.sum = op(t.l.sum, t.sum)
-        if t.r:
-            t.cnt += t.r.cnt
-            t.sum = op(t.sum, t.r.sum)
-        return t
-    
-    def push(self, t):
-        if t.rev:
-            self.toggle(t.l); self.toggle(t.r)
-            t.rev = 0
-        if t.lazy != node_id:
-            self.propagate(t.l, t.lazy); self.propagate(t.r, t.lazy)
-            t.lazy = node_id
-        
-        
-node_e = 0
-node_id = 1<<30
-class LazyReversibleTreapNode:
-    def __init__(self, e=node_e):
-        self.l = None
-        self.r = None
-        self.key = e
-        self.sum = e
-        self.lazy = node_id
-        self.cnt = 1
-        self.rev = 0
-        self.pr = rng()
-
-MOD = 998244353
-MASK = 0x3fffffff
-def op(x, y):#node1.sum * node2.sum
-    a, b, c, d = x>>30, x&MASK, y>>30, y&MASK
-    e = a+c; f = b+d
-    if e >= MOD: e -= MOD
-    if f >= MOD: f -= MOD
-    return e<<30|f
-
-def mapping(a, F):#(node.key or node.sum) * lazy ->(node.key or node.sum)
-    a, b, c, d = a>>30, a&MASK, F>>30, F&MASK
-    e = (c*a + d*b)%MOD; f = b
-    return e<<30|f
-
-def composition(F, G):# lazy1 * lazy2
-    a, b, c, d = F>>30, F&MASK, G>>30, G&MASK
-    e = a*c%MOD; f = (b*c+d)%MOD
-    return e<<30|f
-def ts(a): return a
-
-filename = 'extreme_insertion_01.in'
-with open(filename) as f:
-    firstloop = True
-    secondloop = False
-    for line in f:
-        if firstloop:
-            firstloop = False
-            secondloop = True
-            N, Q = map(int,line.split())
-        elif secondloop:
-            secondloop = False
-            A = list(map(int,line.split()))
-            A = [a<<30|1 for a in A]
-            rbst = LazyReversibleTreap()
-            root = rbst.build(A)
-        else:
-            cmd, *arg = map(int, line.split())
-            if cmd == 0:
-                i, x = arg
-                root = rbst.insert(root, i, x<<30|1)
-            elif cmd == 1:
-                i = arg[0]
-                root = rbst.erase(root, i)
-            elif cmd == 2:
-                l, r = arg
-                root = rbst.reverse(root, l, r)
-            elif cmd == 3:
-                l, r, b, c = arg
-                root = rbst.apply(root, l, r, b<<30|c)
+    def build(self, arr: Iterable[int]) -> None:
+        n = len(arr)
+        ps = [LazyReversibleTreapNode(x, self.id) for x in arr]
+        p = [-1] * n
+        st = []
+        for i in range(n):
+            prv = -1
+            while st and ps[i].pr > ps[st[-1]].pr: prv = st.pop()
+            if prv != -1: p[prv] = i
+            if st: p[i] = st[-1]
+            st.append(i)
+        root = -1
+        for i in range(n):
+            if p[i] != -1:
+                if i < p[i]: ps[p[i]].l = ps[i]
+                else: ps[p[i]].r = ps[i]
             else:
-                l, r = arg
-                res, root = rbst.fold(root, l, r)
-                wtn(res>>30)
-end = time()
-wtn(end-start)
+                root = i
+        self._dfs(ps[root])
+        self.root = ps[root]
 
-# N, Q = rd(), rd()
-# A = [rd()<<30|1 for _ in range(N)]
-# rbst = LazyReversibleRBST()
-# root = rbst.build(A)
+    def insert(self, t: LazyReversibleTreapNode, k: int, e: int) -> None:
+        '''
+        insert new node to kth_index of t
+        t: reference node
+        k: index
+        e: value
+        '''
+        x1, x2 = self._split(t, k)
+        self.root = self._merge(self._merge(x1, LazyReversibleTreapNode(e, self.id)), x2)
 
-# for _ in range(Q):
-#     cmd = rd()
-#     if cmd == 0:
-#         i, x = rd(), rd()
-#         root = rbst.insert(root, i, x<<30|1)
-#     elif cmd == 1:
-#         i = rd()
-#         root = rbst.erase(root, i)
-#     elif cmd == 2:
-#         l, r = rd(), rd()
-#         root = rbst.reverse(root, l, r)
-#     elif cmd == 3:
-#         l, r, b, c = rd(), rd(), rd(), rd()
-#         root = rbst.apply(root, l, r, b<<30|c)
-#     else:
-#         l, r = rd(), rd()
-#         res, root = rbst.fold(root, l, r)
-#         wtn(res>>30)
+    def erase(self, t: LazyReversibleTreapNode, k: int) -> None:
+        '''
+        erase kth_indexed_node of t
+        t: reference node
+        k: index
+        '''
+        x1, x2 = self._split(t, k)
+        self.root = self._merge(x1, self._split(x2, 1)[1])
+
+    def reverse(self, t: LazyReversibleTreapNode, l: int, r: int) -> None:
+        '''
+        reverse [l, r) of t
+        t: reference node
+        l: left
+        r: right
+        '''
+        x1, x2 = self._split(t, l)
+        y1, y2 = self._split(x2, r-l)
+        self._toggle(y1)
+        self.root = self._merge(x1, self._merge(y1, y2))
+
+    def apply(self, t: LazyReversibleTreapNode, l: int, r: int, F: int) -> None:
+        '''
+        apply F to [l, r) of t
+        t: reference node
+        l: left
+        r: right
+        F: operator
+        '''
+        x1, x2 = self._split(t, l)
+        y1, y2 = self._split(x2, r-l)
+        self._propagate(y1, F)
+        self.root = self._merge(x1, self._merge(y1, y2))
+
+    def fold(self, t: LazyReversibleTreapNode, l: int, r: int) -> int:
+        '''
+        product [l, r) of t
+        t: reference node
+        l: left
+        r: right
+        return: sum
+        '''
+        x1, x2 = self._split(t, l)
+        y1, y2 = self._split(x2, r-l)
+        res = self._sum(y1)
+        self.root = self._merge(x1, self._merge(y1, y2))
+        return res
