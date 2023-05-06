3dc0a9fc05a04f41a8f999212e0bd59158acf9df
abUmat <cryptid.ojin@gmail.com>
2023-05-07 02:33:08 +0900

    Update use prime_enumerate to fasten

--- mymath/totient.py
+++ mymath/totient.py
@@ -1,10 +1,14 @@
+# my module
+from prime.prime_enumerate import *
+# my module
 def totient(n: int) -> int:
     "count x in [1, n) s.t. gcd(x, n) == 1"
+    ps = prime_enumerate(n)
     res = n
-    for i in range(2, n + 1):
-        if i * i > n: break
-        if not n % i:
-            res -= res //i
-            while not n % i: n //= i
+    for p in ps:
+        if p * p > n: break
+        if not n % p:
+            res -= res // p
+            while not n % p: n //= p
     if n != 1: res -= res // n
     return res
