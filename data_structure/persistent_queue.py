ac15d3c4829988fecd00243acabb776477d5c58e
abUmat <cryptid.ojin@gmail.com>
2023-05-07 00:49:47 +0900

    Modify ctz(x)

--- data_structure/persistent_queue.py
+++ data_structure/persistent_queue.py
@@ -1,18 +1,22 @@
+from typing import List, Tuple
+# my module
+from gcc_builtins import *
+# my module
+class _PersistentQueueNode:
+    def __init__(self, d: int, val: int, n: List) -> None:
+        self.d = d
+        self.val = val
+        self.par = n
+
 class PersistentQueue:
-    class Node:
-        def __init__(self, d, val, n) -> None:
-            self.d = d
-            self.val = val
-            self.par = n
+    def __init__(self, e: int) -> None:
+        self.root = _PersistentQueueNode(0, e, [])
+        self.start = [self.root]
+        self.end = [self.root]
 
-    def __init__(self, e) -> None:
-        root = self.root = self.Node(0, e, [])
-        self.start = [root]
-        self.end = [root]
-
-    def push(self, val, id_=-1):
+    def push(self, val: int, id_: int=-1) -> int:
         s = self.start[id_]; e = self.end[id_]
-        ne = self.Node(e.d + 1, val, [e])
+        ne = _PersistentQueueNode(e.d + 1, val, [e])
         self.start.append(s); self.end.append(ne)
         for i in range(0x100000):
             if len(e.par) <= i: break
@@ -20,12 +24,13 @@ class PersistentQueue:
             ne.par.append(e)
         return len(self.start) - 1
 
-    def pop(self, id_=-1):
+    def pop(self, id_: int=-1) -> Tuple[int, int]:
         s = self.start[id_]; e = self.end[id_]
         ns = e
         x = e.d - s.d - 1
-        ds = []
-        for i in range(x.bit_length()):
-            if x>>i & 1: ns = ns.par[i]
+        while x:
+            d = ctz(x)
+            ns = ns.par[d]
+            x ^= 1 << d
         self.start.append(ns); self.end.append(e)
         return ns.val, len(self.start) - 1
