418653eba9856a292b7f94ce4d3edb1278947ad2
abUmat <cryptid.ojin@gmail.com>
2023-05-07 01:56:06 +0900

    Update ref. nyaan's library

--- graph/strongly_connected_components.py
+++ graph/strongly_connected_components.py
@@ -1,66 +1,59 @@
-import sys
-# https://qiita.com/AkariLuminous/items/a2c789cebdd098dcb503
-def csr(n, E):
-    start = [0] * (n + 1)
-    elist = [0] * len(E)
-    # start[i+1] = 頂点 i を始点とする辺の数
-    for e0, e1 in E:
-        start[e0 + 1] += 1
-    #累積和
-    for i in range(1, n + 1):
-        start[i] += start[i - 1]
-    #挿入位置を表すポインタ
-    counter = start[:]
-    for e0, e1 in E:
-        elist[counter[e0]] = e1
-        counter[e0] += 1 # ポインタを進める
-    return start, elist
+from typing import List
+class StronglyConnectedComponents:
+    _order: List[int] = []
+    def __init__(self, G):
+        self._g = G
+        self._used = [0] * len(G)
+        self._build()
 
-class SCC:
-    def __init__(self, n):
-        self._n = n
-        self.edges = []
-        sys.setrecursionlimit(max(2*n, sys.getrecursionlimit()))
+    def __getitem__(self, k: int) -> int:
+        return self._comp[k]
 
-    def num_vertices(self): return self._n
+    def belong(self, i: int) -> List[int]:
+        return self._blng[i]
 
-    def add(self, frm, to): self.edges.append([frm, to])
+    def _dfs(self, s: int) -> None:
+        q = [s]
+        while q:
+            v = q.pop()
+            if v >= 0:
+                if self._used[v]: continue
+                q.append(~v)
+                self._used[v] = 1
+                for vv in self._g[v]:
+                    q.append(vv)
+            else:
+                self._order.append(~v)
 
-    def scc(self):
-        group_num, ids = self.scc_ids()
-        groups = [[] for _ in range(group_num)]
-        for i in range(self._n): groups[ids[i]].append(i)
-        return groups
+    def _rdfs(self, s: int, cnt: int) -> None:
+        q = [s]
+        while q:
+            v = q.pop()
+            if v >= 0:
+                if self._comp[v] != -1: continue
+                self._comp[v] = cnt
+                for vv in self._rg[v]:
+                    q.append(vv)
 
-    def scc_ids(self):
-        start, elist = csr(self._n, self.edges)
-        now_ord, group_num = 0, 0
-        visited = []
-        low = [0] * self._n
-        ord_ = [-1] * self._n
-        ids = [0] * self._n
-
-        def dfs(v):
-            nonlocal now_ord, group_num, visited, low, ord_, ids
-            low[v] = ord_[v] = now_ord
-            now_ord += 1
-            visited.append(v)
-            for i in range(start[v], start[v+1]):
-                to = elist[i]
-                if ord_[to] == -1:
-                    dfs(to)
-                    low[v] = min(low[v], low[to])
-                else:
-                    low[v] = min(low[v], ord_[to])
-            if low[v] == ord_[v]:
-                while True:
-                    u = visited.pop()
-                    ord_[u] = self._n
-                    ids[u] = group_num
-                    if u == v: break
-                group_num += 1
-
-        for i in range(self._n):
-            if ord_[i] == -1: dfs(i)
-        for i in range(self._n): ids[i] = group_num - 1 - ids[i]
-        return group_num, ids
+    def _build(self):
+        for v in range(len(self._g)): self._dfs(v)
+        self._order.reverse()
+        self._comp = comp = [-1] * len(self._g)
+        self._rg = rg = [[] for _ in range(len(self._g))]
+        for v in range(len(self._g)):
+            for vv in self._g[v]:
+                rg[vv].append(v)
+        ptr = 0
+        for v in self._order:
+            if comp[v] == -1:
+                self._rdfs(v, ptr)
+                ptr += 1
+        self.dag = [[] for _ in range(ptr)]
+        self._blng = [[] for _ in range(ptr)]
+        for v in range(len(self._g)):
+            self._blng[comp[v]].append(v)
+            for vv in self._g[v]:
+                x = comp[v]
+                y = comp[vv]
+                if x == y: continue
+                self.dag[x].append(y)
