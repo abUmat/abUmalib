5b1878dca60e961961a1977fc0dd18e4c97ea6c8
abUmat <cryptid.ojin@gmail.com>
2023-05-08 01:31:28 +0900

    Update non-recursive

--- multiplicative_function/sum_of_totient.py
+++ multiplicative_function/sum_of_totient.py
@@ -1,6 +1,5 @@
 def sum_of_totient(N: int, mod: int) -> int:
     if N < 2: return N
-    f = lambda v, p, c: v // p * (p - 1)
     ns = [0]; p = []
     i = N
     while i > 0:
@@ -8,36 +7,33 @@ def sum_of_totient(N: int, mod: int) -> int:
         i = N // (N // i + 1)
     s = len(ns)
     sq = int(N ** 0.5)
-    idx = lambda n: s - n if n <= sq else N // n
 
-    h0 = [(x - 1) % mod for x in ns]
-    h1 = [((x * (x + 1) >> 1) - 1) % mod for x in ns]
-
+    h0 = [x - 1 for x in ns]
+    h1 = [(x * (x + 3) >> 1) % mod for x in h0]
     for x in range(2, sq + 1):
-        if h0[s - x] == h0[s - x + 1]: continue
+        if h0[-x] == h0[-x + 1]: continue
         p.append(x)
         x2 = x * x
-        for i in range(1, s):
-            n = ns[i]
+        h0tmp, h1tmp = h0[-x + 1], h1[-x + 1]
+        for i, n in enumerate(ns):
+            if not i: continue
             if n < x2: break
             id = i * x if i * x <= sq else s - n // x
-            h0[i] -= h0[id] - h0[s - x + 1]
-            h1[i] -= (h1[id] - h1[s - x + 1]) * x % mod
-    buf = [(y - x) % mod for x, y in zip(h0, h1)]
-    ans = buf[idx(N)] + 1
+            h0[i] -= h0[id] - h0tmp
+            h1[i] -= (h1[id] - h1tmp) * x
+    buf = [y - x for x, y in zip(h0, h1)]
+    ans = buf[1] + 1
+    idx = lambda n: s - n if n <= sq else N // n
 
-    def dfs(i: int, c: int, v: int, lim: int, cur: int) -> None:
-        nonlocal ans
-        ans += cur * f(p[i] * v, p[i], c + 1) % mod
-        if lim >= p[i] * p[i]:
-            dfs(i, c + 1, p[i] * v, lim // p[i], cur)
-        cur *= f(v, p[i], c)
-        ans += cur * (buf[idx(lim)] - buf[idx(p[i])]) % mod
+    q = [[i, x, x, N // x, 1] for i, x in enumerate(p)]
+    while q:
+        i, x, val, lim, v = q.pop()
+        if lim >= x * x:
+            q.append([i, x, x * val, lim // x, v])
+        vv = v * (val // x) * (x - 1)
+        ans += (vv * (buf[idx(lim)] - buf[idx(x)]) + v * (val * x - val))
         for j in range(i + 1, len(p)):
-            if p[j] * p[j] > lim: break
-            dfs(j, 1, p[j], lim // p[j], cur)
-
-    for i in range(len(p)):
-        dfs(i, 1, p[i], N // p[i], 1)
-
+            x = p[j]
+            if x * x > lim: break
+            q.append([j, x, x, lim // x, vv])
     return ans % mod
