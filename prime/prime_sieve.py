2e31f86e7c97d11b8e4127540363f7cc04006ce0
abUmat <cryptid.ojin@gmail.com>
2023-05-10 22:41:07 +0900

    Update prime_sieve.py use bytearray

--- prime/prime_sieve.py
+++ prime/prime_sieve.py
@@ -1,6 +1,6 @@
 def prime_sieve(n):
     'O(nloglogn) Sieve of Eratosthenes'
-    res = [1] * (n+1)
+    res = bytearray([1] * (n+1))
     res[0] = res[1] = 0
     for i in range(2,n):
         if i*i > n: break
