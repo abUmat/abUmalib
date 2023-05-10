f5334278ae29fb23d23915c35a19a7964da93eb0
abUmat <cryptid.ojin@gmail.com>
2023-05-10 22:29:06 +0900

    Update mo.solve

--- misc/mo.py
+++ misc/mo.py
@@ -1,32 +1,32 @@
 class Mo:
     def __init__(self, Q, add_left, add_right, remove_left, remove_right, get):
-        self.q = Q
-        self.query = [0] * Q
-        self.data = [0] * Q
-        self.add_left = add_left
-        self.add_right = add_right
-        self.remove_left = remove_left
-        self.remove_right = remove_right
-        self.get = get
+        self._q = Q
+        self._query = [0] * Q
+        self._data = [0] * Q
+        self._add_left = add_left
+        self._add_right = add_right
+        self._remove_left = remove_left
+        self._remove_right = remove_right
+        self._get = get
 
     def add_query(self, l, r, i, W):
-        self.data[i] = l<<20|r
-        self.query[i] = ((l//W)<<40)+((-r if (l//W)&1 else r)<<20)+i
+        self._data[i] = l << 20 | r
+        self._query[i] = (int(l / W) << 40) + ((-r if int(l / W) & 1 else r) << 20) + i
 
     def solve(self):
-        self.query.sort()
+        add_left = self._add_left
+        add_right = self._add_right
+        remove_left = self._remove_left
+        remove_right = self._remove_right
+        get = self._get
+        self._query.sort()
+        pop = self._query.pop
         L, R = 0, 0
-        res = [0] * self.q
-        mask = (1<<20)-1
-        add_left = self.add_left
-        add_right = self.add_right
-        remove_left = self.remove_left
-        remove_right = self.remove_right
-        get = self.get
-        for lri  in self.query:
-            i = lri&mask
-            lr = self.data[i]
-            l, r = lr>>20, lr&mask
+        res = [0] * self._q
+        for _ in range(self._q):
+            i = pop() & 0xfffff
+            lr = self._data[i]
+            l, r = lr >> 20, lr & 0xfffff
             while L > l:
                 L -= 1
                 add_left(L)
