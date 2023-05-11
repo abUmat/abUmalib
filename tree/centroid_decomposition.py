bfab20d2bcd3e9c6d817f27012bc9699b2df3301
abUmat <cryptid.ojin@gmail.com>
2023-05-12 02:58:08 +0900

    Add centroid_decomposition.py and freq_of_dist.py

---
+++ tree/centroid_decomposition.py
@@ -0,0 +1,50 @@
+class CentroidDecomposition:
+    def __init__(self, G, isbuild=1) -> None:
+        self.g = G
+        self.sub = [0] * len(G)
+        self.v = bytearray(len(G))
+        if isbuild: self.build()
+
+    def build(self) -> None:
+        self.tree = [[] for _ in range(len(self.g))]
+        self.root = self.build_dfs(0)
+
+    def get_size(self, _cur: int, _par: int) -> int:
+        q = [[_cur, _par]]
+        while q:
+            cur, par = q.pop()
+            if cur >= 0:
+                q.append([~cur, par])
+                self.sub[cur] = 1
+                for dst in self.g[cur]:
+                    if dst == par or self.v[dst]: continue
+                    q.append([dst, cur])
+            else:
+                if par == -1: return self.sub[~cur]
+                self.sub[par] += self.sub[~cur]
+
+    def get_centroid(self, _cur: int, _par: int, mid: int) -> int:
+        q = [[_cur, _par]]
+        ans = -1
+        while q:
+            cur, par = q.pop()
+            if cur >= 0:
+                q.append([~cur, par])
+                for dst in self.g[cur]:
+                    if dst == par or self.v[dst]: continue
+                    if self.sub[dst] > mid:
+                        q.append([dst, cur])
+                        break
+            else:
+                if ans == -1: ans = ~cur
+                if par == -1: return ans
+
+    def build_dfs(self, cur: int) -> int:
+        centroid = self.get_centroid(cur, -1, self.get_size(cur, -1) >> 1)
+        self.v[centroid] = 1
+        for dst in self.g[cur]:
+            if not self.v[dst]:
+                nxt = self.build_dfs(dst)
+                if centroid != nxt: self.tree[centroid].append(nxt)
+        self.v[centroid] = 0
+        return centroid
---
+++ tree/frequency_table_of_tree_distance.py
@@ -0,0 +1,48 @@
+from collections import deque
+# my module
+from tree.centroid_decomposition import *
+from ntt.arbitrary_ntt import *
+# my module
+class FrequencyTableOfTreeDistance(CentroidDecomposition):
+    def __init__(self, g) -> None:
+        super().__init__(g, 0)
+
+    def _dfs_depth(self, cur: int, par: int, d: int, count: List[int], self_: List[int]) -> None:
+        mask = (1 << 20) - 1
+        q = [cur << 40 | par << 20 | d]
+        while q:
+            x = q.pop()
+            cur, par, d = x >> 40, x >> 20 & mask, x & mask
+            count[len(count):] = [0] * (d + 1 - len(count))
+            self_[len(self_):] = [0] * (d + 1 - len(self_))
+            count[d] += 1
+            self_[d] += 1
+            for dst in self.g[cur]:
+                if par == dst or self.v[dst]: continue
+                q.append(dst << 40 | cur << 20 | (d + 1))
+
+    def get(self, start: int=0) -> List[int]:
+        pow2 = ArbitraryNTT.pow2_u128
+        get_centroid, get_size = self.get_centroid, self.get_size
+        Q: deque[int] = deque()
+        root = get_centroid(start, -1, get_size(start, -1) >> 1)
+        Q.append(root)
+        ans: List[int] = [0] * (len(self.g) + 1)
+        while Q:
+            r = Q.popleft()
+            count = []
+            self.v[r] = 1
+            for c in self.g[r]:
+                if self.v[c]: continue
+                self_ = []
+                Q.append(get_centroid(c, -1, get_size(c, -1) >> 1))
+                self._dfs_depth(c, r, 1, count, self_)
+                self2 = pow2(self_)
+                for i, x in enumerate(self2): ans[i] -= x
+            if not count: continue
+            count[0] += 1
+            count2 = pow2(count)
+            for i, x in enumerate(count2): ans[i] += x
+        for i in range(len(ans)): ans[i] >>= 1
+        return ans
+
