e56fd70cd25948b22d71a9190309d958f77aa2e7
abUmat <cryptid.ojin@gmail.com>
2023-05-13 20:38:50 +0900

    Update add dijkstra_point and dijkstra_restore

--- shortest_path/dijkstra_fast.py
+++ shortest_path/dijkstra_fast.py
@@ -1,11 +1,10 @@
 # my module
 from data_structure.radix_heap import *
 # my module
-def dijkstra(g, s):
-    mask = (1 << 30) - 1
-    n = len(g)
+def dijkstra(g, s: int) -> list:
     INF = (1 << 60) - 1
-    dist = [INF] * n
+    MASK = (1 << 30) - 1
+    dist = [INF] * len(g)
     heap = RadixHeap()
     heap.push(0, s)
     dist[s] = 0
@@ -13,8 +12,43 @@ def dijkstra(g, s):
         d, v = heap.pop()
         if d > dist[v]: continue
         for tmp in g[v]:
-            vv, w = tmp >> 30, tmp & mask
+            vv, w = tmp >> 30, tmp & MASK
             if dist[vv] > d + w:
                 dist[vv] = d + w
-                heap.push(d + w ,vv)
+                heap.push(d + w, vv)
+    return dist
+
+def dijkstra_point(g, start: int, goal: int) -> int:
+    INF = (1 << 60) - 1
+    MASK = (1 << 30) - 1
+    dist = [INF] * len(g)
+    heap = RadixHeap()
+    dist[start] = 0
+    heap.push(0, start)
+    while heap:
+        d, v = heap.pop()
+        if v == goal: return dist[v]
+        if d > dist[v]: continue
+        for tmp in g[v]:
+            vv, w = tmp >> 30, tmp & MASK
+            if dist[vv] > d + w:
+                dist[vv] = d + w
+                heap.push(d + w, vv)
+    return -1
+
+def dijkstra_restore(g, start: int=0):
+    INF = (1 << 60) - 1
+    MASK = (1 << 30) - 1
+    dist = [(INF, -1) for _ in range(len(g))]
+    heap = RadixHeap()
+    dist[start] = 0, -1
+    heap.push(0, start)
+    while heap:
+        d, v = heap.pop()
+        if d > dist[v][0]: continue
+        for tmp in g[v]:
+            vv, w = tmp >> 30, tmp & MASK
+            if dist[vv][0] > d + w:
+                dist[vv] = d + w, v
+                heap.push(d + w, vv)
     return dist
