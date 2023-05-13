49ff0c90a8fa329374443d124b6d85d6afdde219
abUmat <cryptid.ojin@gmail.com>
2023-05-13 19:51:54 +0900

    Add dijkstra.py

---
+++ shortest_path/dijkstra_fast.py
@@ -0,0 +1,20 @@
+# my module
+from data_structure.radix_heap import *
+# my module
+def dijkstra(g, s):
+    mask = (1 << 30) - 1
+    n = len(g)
+    INF = (1 << 60) - 1
+    dist = [INF] * (n + 1)
+    heap = RadixHeap()
+    heap.push(0, s)
+    dist[s] = 0
+    while heap:
+        d, v = heap.pop()
+        if d > dist[v]: continue
+        for tmp in g[v]:
+            vv, w = tmp >> 30, tmp & mask
+            if dist[vv] > d + w:
+                dist[vv] = d + w
+                heap.push(d + w ,vv)
+    return dist
