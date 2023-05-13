a81605cfa3e46c98a110e4dc539b8fc39cf1caa8
abUmat <cryptid.ojin@gmail.com>
2023-05-14 00:53:12 +0900

    Update two_edge_cc.dfs non-recursive

--- graph/two_edge_connected_components.py
+++ graph/two_edge_connected_components.py
@@ -9,7 +9,7 @@ class TwoEdgeConnectedComponents:
         self.comp = comp = [-1] * N
         self.k = 0
         for i in range(N):
-            if comp[i] == -1: self._dfs(i, -1)
+            if comp[i] == -1: self._dfs(i)
         self.groups = groups = [[] for _ in range(self.k)]
         self.tree = tree = [[] for _ in range(self.k)]
         for i, x in enumerate(comp): groups[x].append(i)
@@ -20,16 +20,16 @@ class TwoEdgeConnectedComponents:
     def __getitem__(self, k: int) -> int:
         return self.comp[k]
 
-    def _dfs(self, i: int, p: int) -> None:
-        q = [[i, p]]
+    def _dfs(self, i: int) -> None:
+        q = [i << 20 | i]
         while q:
-            i, p = q.pop()
-            if p >= 0 and self.low.ord[p] >= self.low.low[i]:
+            tmp = q.pop()
+            i, p = tmp >> 20, tmp & 0xfffff
+            if i != p and self.low.ord[p] >= self.low.low[i]:
                 self.comp[i] = self.comp[p]
             else:
                 self.comp[i] = self.k
                 self.k += 1
             for d in self.g[i]:
                 if self.comp[d] == -1:
-                    q.append([d, i])
-
+                    q.append(d << 20 | i)
