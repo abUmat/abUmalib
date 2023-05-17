99d4c39cf57bbe044949328f01fb8bd593d9f84f
abUmat <cryptid.ojin@gmail.com>
2023-05-18 02:05:50 +0900

    Update try to fast for-loop by use tmp var

--- fps/lagrange_interpolation_point.py
+++ fps/lagrange_interpolation_point.py
@@ -1,18 +1,20 @@
 # my module
 from modulo.binomial import *
 # my module
-def lagrange_interpolation(y: List[int], x: int, C: Binomial, mod: int) -> int:
+def lagrange_interpolation(y: list, x: int, C: Binomial, mod: int) -> int:
     N = len(y) - 1
     if x <= N: return y[x] % mod
     res = 0
     dp = [1] * (N + 1)
-    pd = [1] * (N + 1)
     x %= mod
+    tmp = 1
     for i in range(N):
-        dp[i + 1] = dp[i] * x % mod
+        dp[i + 1] = tmp = tmp * x % mod
         x -= 1
+    pd = [1] * (N + 1)
+    tmp = 1
     for i in range(N, 0, -1):
-        pd[i - 1] = pd[i] * x % mod
+        pd[i - 1] = tmp = tmp * x % mod
         x += 1
     for i in range(N + 1):
         tmp = (((y[i] * dp[i] % mod) * pd[i] % mod) * C.finv(i) % mod) * C.finv(N - i) % mod
