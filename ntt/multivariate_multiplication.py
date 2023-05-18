decde14067d30f3fd339077d8e45015cb8183dc1
abUmat <cryptid.ojin@gmail.com>
2023-05-18 20:46:17 +0900

    Update delete zip and add docstring()

--- ntt/multivariate_multiplication.py
+++ ntt/multivariate_multiplication.py
@@ -1,10 +1,11 @@
+# https://nyaannyaan.github.io/library/ntt/multivariate-multiplication.hpp
 # my module
 from ntt.ntt import *
 # my module
-def multivariate_multiplication(f: List[int], g: List[int], base: List[int], mod: int) -> List[int]:
+def multivariate_multiplication(f: list, g: list, base: list, mod: int) -> list:
+    '''ntt friendly only'''
     n = len(f); s = len(base); W = 1
-    if s == 0:
-        return [f[0] * g[0] % mod]
+    if s == 0: return [f[0] * g[0] % mod]
     while W < n << 1: W <<= 1
     chi = [0] * n
     for i in range(n):
@@ -15,17 +16,18 @@ def multivariate_multiplication(f: List[int], g: List[int], base: List[int], mod
         chi[i] %= s
     F = [[0] * W for _ in range(s)]
     G = [[0] * W for _ in range(s)]
-    for i, (j, x, y) in enumerate(zip(chi, f, g)):
-        F[j][i] = x
-        G[j][i] = y
+    for i, j in enumerate(chi):
+        F[j][i] = f[i]
+        G[j][i] = g[i]
     ntt = NTT(mod)
-    for i, (f, g) in enumerate(zip(F, G)): ntt.ntt(f); ntt.ntt(g)
+    for i in range(s): ntt.ntt(F[i]); ntt.ntt(G[i])
     for k in range(W):
         a = [0] * s
         for i, f in enumerate(F):
+            tmp = f[k]
             for j, g in enumerate(G):
-                a[i + j - (s if i + j >= s else 0)] += f[k] * g[k] % mod
+                a[i + j - (s if i + j >= s else 0)] += tmp * g[k] % mod
         for i, f in enumerate(F):
             f[k] = a[i] % mod
-    for i, f in enumerate(F): ntt.intt(f)
+    for f in F: ntt.intt(f)
     return [F[j][i] for i, j in enumerate(chi)]
