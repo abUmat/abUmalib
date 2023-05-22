b2dd4e31675028990d5fd3a5eec7cdc8cef02a63
abUmat <cryptid.ojin@gmail.com>
2023-05-23 00:24:27 +0900

    Add fps-composition-inv

---
+++ fps/fps_composition_inverse.py
@@ -0,0 +1,53 @@
+# my module
+from fps.fps_composition import *
+# my module
+# https://judge.yosupo.jp/submission/139183
+def composition_inverse(f: list, mod: int, deg: int=-1) -> None:
+    def composition_multi(Ps: list, Q: list, deg: int) -> list:
+        k = int(deg ** .5+ 1)
+        d = (deg + k) // k
+
+        X = [[] for _ in range(k + 1)]
+        X[0] = [1]
+        for i, x in enumerate(X):
+            if i == k: break
+            X[i + 1] = fps.mul(x, Q)
+            X[i + 1][deg + 1:] = []
+        leny = len(X[-1])
+        X[d + 1:] = []
+        xd = X.pop()
+
+        ress = []
+        for P in Ps:
+            lenp = len(P)
+            Z = [1]
+            F = [0] * (deg + 1)
+            for i in range(k):
+                y = [0] * leny
+                for j, x in enumerate(X):
+                    if i * d + j >= lenp: break
+                    for t, xx in enumerate(x):
+                        y[t] += xx * P[i * d + j] % mod
+                y = fps.mul(y, Z)
+                y[deg + 1:] = []
+                for j, yy in enumerate(y):
+                    F[j] += yy
+                Z = fps.mul(Z, xd)
+                Z[deg + 1:] = []
+            F.pop()
+            ress.append([x % mod for x in F])
+        return ress
+
+    fps = FPS(mod)
+    deg = len(f) if deg == -1 else deg
+    dfdx = fps.diff(f)
+    f = [-x for x in f]
+    res = [0]
+    m = 1
+    while m < deg:
+        m <<= 1
+        cf0, cf1 = composition_multi([f, dfdx], res, m)
+        cf0[1] += 1
+        tmp = fps.mul(cf0, fps.inv(cf1, m))
+        res[m >> 1:] = tmp[m >> 1:min(deg, m)]
+    return res
---
+++ math998244353/fps_composition_inverse.py
@@ -0,0 +1,17 @@
+# my module
+from math998244353.fps_composition import *
+# my module
+# https://judge.yosupo.jp/submission/139183
+def composition_inverse(f: list, deg: int=-1) -> None:
+    deg = len(f) if deg == -1 else deg
+    dfdx = FPS.diff(f)
+    f = [-x for x in f]
+    res = [0]
+    m = 1
+    while m < deg:
+        m <<= 1
+        cf0, cf1 = composition_multi([f, dfdx], res, m)
+        cf0[1] += 1
+        tmp = NTT.multiply(cf0, FPS.inv(cf1, m))
+        res[m >> 1:] = tmp[m >> 1:min(deg, m)]
+    return res
