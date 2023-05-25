930b7c5678e1f3aeb3f71a8002f8e41c52e884e4
abUmat <cryptid.ojin@gmail.com>
2023-05-25 19:39:21 +0900

    Update modify a little

--- prime/fast_factorize.py
+++ prime/fast_factorize.py
@@ -4,10 +4,10 @@ from prime.is_prime import *
 from mymath.gcd_lcm import *
 # my module
 # https://nyaannyaan.github.io/library/prime/fast-factorize.hpp
-def pollard_rho(n):
+def pollard_rho(n: int) -> int:
     b = n.bit_length()-1
-    b = (b>>2)<<2
-    m = int(2**(b/8))<<1
+    b = (b >> 2) << 2
+    m = int(2 ** (b / 8)) << 1
     while True:
         c = randrange(1, n)
         f = lambda a: (a * a + c) % n
@@ -19,9 +19,9 @@ def pollard_rho(n):
             k = 0
             while k < r and g == 1:
                 ys = y
-                for _ in range(min(m, r-k)):
+                for _ in range(min(m, r - k)):
                     y = f(y)
-                    q = q*abs(x-y)%n
+                    q = q * (x - y if x > y else y - x) % n
                 g = gcd2(q, n)
                 k += m
             r <<= 1
@@ -30,26 +30,27 @@ def pollard_rho(n):
             y = ys
             while g == 1:
                 y = f(y)
-                g = gcd2(abs(x-y), n)
+                g = gcd2((x - y if x > y else y - x) , n)
         if g == n: continue
         if is_prime(g): return g
-        elif is_prime(n//g): return n//g
+        elif is_prime(n // g): return n // g
         else: n = g
+
 def factorize(n):
     'O(N**0.25) pollard rho algorithm'
     res = {}
     for p in range(2,1000):
-        if p*p > n: break
-        if n%p: continue
+        if p * p > n: break
+        if n % p: continue
         s = 0
-        while n%p == 0:
+        while n % p == 0:
             n //= p
             s += 1
         res[p] = s
     while not is_prime(n) and n > 1:
         p = pollard_rho(n)
         s = 0
-        while n%p == 0:
+        while n % p == 0:
             n //= p
             s += 1
         res[p] = s
