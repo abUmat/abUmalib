9d1e14063ceae93fb4b8318d768783c92f06bd1e
abUmat <cryptid.ojin@gmail.com>
2023-05-26 23:41:27 +0900

    Update and Add EulerTour and etc.

--- tree/euler_tour.py
+++ tree/euler_tour.py
@@ -1,30 +1,89 @@
-def euler_tour(root, G):
-    N = len(G)
-    ete = []
-    etv = []
-    parents = [None] * N
-    in_ = [0] * N
-    out = [0] * N
-    depth = [None]*N
-    q = [~root, root]
-    depth[root] = 0
-    i = 0
-    while q:
-        v = q.pop()
-        if v >= 0:
-            ete.append(v)
-            etv.append(v)
-            in_[v] = i
-            for vv in G[v]:
-                if depth[vv] is not None: continue
-                depth[vv] = depth[v] + 1
-                parents[vv] = v
-                q.append(~vv)
-                q.append(vv)
-        else:
-            ete.append(~v)
-            etv.append(parents[~v])
-            out[~v] = i
-        i += 1
-    etv.pop()
-    return ete, etv, in_, out, depth
+# my module
+from data_structure.bit import *
+from data_structure.sparse_table import *
+# my module
+class EulerTour:
+    def __init__(self, g, root: int=0) -> None:
+        '''
+        euler tourを行い初期化
+        ete: 通る辺の順番
+        etv: 通る頂点の順番
+        in_: 頂点を最初に通った時刻
+        out: 頂点を最後に通った時刻
+        depth: 頂点の深さ
+        '''
+        self.n = n = len(g)
+        ete = []
+        etv = []
+        parents = [0] * n
+        in_ = [0] * n
+        out = [0] * n
+        depth = [-1] * n
+        stack = [root]
+        depth[root] = 0
+        i = 0
+        while stack:
+            v = stack.pop()
+            if v >= 0:
+                stack.append(~v)
+                ete.append(v)
+                etv.append(v)
+                in_[v] = i
+                for vv in g[v]:
+                    if depth[vv] != -1: continue
+                    depth[vv] = depth[v] + 1
+                    parents[vv] = v
+                    stack.append(vv)
+            else:
+                ete.append(~v)
+                etv.append(parents[~v])
+                out[~v] = i
+            i += 1
+        etv.pop()
+        self.ete, self.etv = ete, etv
+        self.in_, self.out = in_, out
+        self.depth = depth
+        self.st = SparseTable([depth[v] << 20 | v for v in etv])
+
+    def build(self, arr: list) -> None:
+        '''arrで値を初期化する'''
+        arr_for_path = [0] * (self.n << 1)
+        for i, e in enumerate(self.ete):
+            arr_for_path[i] += arr[e]
+            arr[e] = -arr[e]
+        self.bit_for_path = BIT(self.n << 1, arr_for_path)
+
+        arr_for_subtree = [0] * (self.n << 1)
+        for i, e in enumerate(self.ete):
+            arr_for_subtree[i] += arr[e]
+            arr[e] = 0
+        self.bit_for_subtree = BIT(self.n << 1, arr_for_subtree)
+
+    def lca(self, u: int, v: int) -> int:
+        '''LCA of u and v'''
+        x, y = self.in_[u], self.in_[v]
+        if x > y: x, y = y, x
+        res = self.st.query(x, y + 1)
+        return res & 0xfffff
+
+    def add(self, p: int, x: int) -> int:
+        '''頂点pにxを加える'''
+        self.bit_for_path.add(self.in_[p], x)
+        self.bit_for_path.add(self.out[p], -x)
+        self.bit_for_subtree.add(self.in_[p], x)
+
+    def query_vertex(self, u: int, v: int) -> int:
+        '''頂点に値を持つ場合のpath-sum'''
+        lca = self.lca(u, v)
+        pref = self.bit_for_path.pref
+        return pref(self.in_[u] + 1) + pref(self.in_[v] + 1) - (pref(self.in_[lca] + 1) << 1) + self.bit_for_path[self.in_[lca]]
+
+    def query_edge(self, u: int, v: int) -> int:
+        '''辺に値を持つ場合のpath-sum'''
+        lca = self.lca(u, v)
+        pref = self.bit_for_path.pref
+        return pref(self.in_[u] + 1) + pref(self.in_[v] + 1) - (pref(self.in_[lca] + 1) << 1)
+
+    def query_subtree(self, u: int) -> int:
+        '''頂点uのsubtree-sum'''
+        return self.bit_for_subtree.sum(self.in_[u], self.out[u])
---
+++ tree/euler_tour_func.py
@@ -0,0 +1,37 @@
+def euler_tour(g: list, root: int=0) -> tuple:
+    '''
+    隣接リストと根を与えると
+    通る辺, 通る頂点, 頂点に到達した時刻, 頂点を最後に通った時刻, 頂点の深さ
+    のタプルを返す
+    g: 隣接リスト
+    root: root
+    '''
+    N = len(g)
+    ete = []
+    etv = []
+    parents = [0] * N
+    in_ = [0] * N
+    out = [0] * N
+    depth = [-1] * N
+    stack = [root]
+    depth[root] = 0
+    i = 0
+    while stack:
+        v = stack.pop()
+        if v >= 0:
+            stack.append(~v)
+            ete.append(v)
+            etv.append(v)
+            in_[v] = i
+            for vv in g[v]:
+                if depth[vv] != -1: continue
+                depth[vv] = depth[v] + 1
+                parents[vv] = v
+                stack.append(vv)
+        else:
+            ete.append(~v)
+            etv.append(parents[~v])
+            out[~v] = i
+        i += 1
+    etv.pop()
+    return ete, etv, in_, out, depth
