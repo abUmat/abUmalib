e2c53fce2761f33b8ffdd0624548fe3735002c56
abUmat <cryptid.ojin@gmail.com>
2023-06-11 21:46:05 +0900

    Update add lambda int int: void

--- misc/typing_template.py
+++ misc/typing_template.py
@@ -1,8 +1,10 @@
-from typing import List, Tuple, Dict, Iterable, Sequence, Callable, Union, TypeVar
+from typing import List, Tuple, Dict, Iterable, Sequence, Callable, Union, Optional, TypeVar
 T = TypeVar('T')
 Graph = List[List[int]]
 Poly = List[int]
 Vector = List[int]
 Matrix = List[List[int]]
+Func10 = Callable[[int], None]
+Func20 = Callable[[int, int], None]
 Func21 = Callable[[int, int], int]
 Func31 = Callable[[int, int, int], int]
