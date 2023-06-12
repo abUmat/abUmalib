36760eba7117467f1eb93c703b9c7b0b1627a325
abUmat <cryptid.ojin@gmail.com>
2023-06-13 03:19:48 +0900

    Update add docstring

--- graph/manhattan_mst.py
+++ graph/manhattan_mst.py
@@ -3,39 +3,50 @@ from data_structure.tatyam_sortedset import *
 from data_structure.unionfind import *
 # my module
 class Map:
+    '''I want to use dict like c++ map while transrating c++ library...'''
     def __init__(self) -> None:
         self.keys = SortedSetInt()
         self.dic: Dict[int, int] = {}
 
     def lower_bound(self, k: int) -> int:
+        '''O(N**0.5)'''
         it = self.keys.index(k)
         return it
 
     def erase(self, it: int) -> None:
+        '''O(N**0.5)'''
         key = self.keys[it]
         self.keys.discard(key)
         self.dic.pop(key)
 
     def end(self) -> int:
+        '''O(1)'''
         return len(self.keys)
 
     def __getitem__(self, k: int) -> int:
+        '''O(1)'''
         return self.dic[k]
 
     def __setitem__(self, k: int, v: int) -> int:
+        '''O(N**0.5)'''
         self.keys.add(k)
         self.dic[k] = v
 
     def get_by_iter(self, it: int) -> Tuple[int, int]:
+        '''O(abs(it) / (N ** 0.5))'''
         key = self.keys[it]
         val = self.dic[key]
         return key, val
 
     def get(self, k: int, default: int) -> int:
+        '''O(1)'''
         return self.dic.get(k, default)
 
 
 def manhattan_mst(X: List[int], Y: List[int]) -> Dict[int, int]:
+    '''
+    X, Y: Points
+    return: dict(key = i << 30 | j, value = cost)'''
     assert(len(X) == len(Y))
     N = len(X)
     dat: List[Tuple[int, int]]= []
