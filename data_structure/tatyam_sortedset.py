14a589ec4ae5ed5c316cd054dbf9050f66057185
abUmat <cryptid.ojin@gmail.com>
2023-06-13 02:33:32 +0900

    Update add int-only SortedSet

--- data_structure/tatyam_sortedset.py
+++ data_structure/tatyam_sortedset.py
@@ -1,8 +1,9 @@
+# my module
+from misc.typing_template import *
+# my module
 # https://github.com/tatyam-prime/SortedSet/blob/main/SortedSet.py
 from bisect import bisect_left, bisect_right
-from typing import Generic, Iterable, Iterator, TypeVar, Union, List
-T = TypeVar('T')
-
+from array import array
 class SortedSet(Generic[T]):
     BUCKET_RATIO = 50
     REBUILD_RATIO = 170
@@ -114,6 +115,124 @@ class SortedSet(Generic[T]):
         return ans
 
     def index_right(self, x: T) -> int:
+        "Count the number of elements <= x."
+        ans = 0
+        for a in self.a:
+            if a[-1] > x: return ans + bisect_right(a, x)
+            ans += len(a)
+        return ans
+
+class SortedSetInt:
+    BUCKET_RATIO = 50
+    REBUILD_RATIO = 170
+
+    def _build(self, a=None) -> None:
+        "Evenly divide `a` into buckets."
+        from math import ceil, sqrt
+        if a is None: a = list(self)
+        size = self.size = len(a)
+        bucket_size = int(ceil(sqrt(size / self.BUCKET_RATIO)))
+        self.a = [array('i', a[size * i // bucket_size : size * (i + 1) // bucket_size]) for i in range(bucket_size)]
+
+    def __init__(self, a: Iterable[int] = []) -> None:
+        "Make a new SortedSet from iterable. / O(N) if sorted and unique / O(N log N)"
+        a = list(a)
+        if not all(a[i] < a[i + 1] for i in range(len(a) - 1)): a = sorted(set(a))
+        self._build(a)
+
+    def __iter__(self) -> Iterator[int]:
+        for i in self.a:
+            for j in i: yield j
+
+    def __reversed__(self) -> Iterator[int]:
+        for i in reversed(self.a):
+            for j in reversed(i): yield j
+
+    def __len__(self) -> int:
+        return self.size
+
+    def __repr__(self) -> str:
+        return "SortedSet" + str(self.a)
+
+    def __str__(self) -> str:
+        s = str(list(self))
+        return "{" + s[1 : len(s) - 1] + "}"
+
+    def _find_bucket(self, x: int) -> List[int]:
+        "Find the bucket which should contain x. self must not be empty."
+        for a in self.a:
+            if x <= a[-1]: return a
+        return a
+
+    def __contains__(self, x: int) -> bool:
+        if self.size == 0: return False
+        a = self._find_bucket(x)
+        i = bisect_left(a, x)
+        return i != len(a) and a[i] == x
+
+    def add(self, x: int) -> bool:
+        "Add an element and return True if added. / O(√N)"
+        if self.size == 0:
+            self.a = [[x]]
+            self.size = 1
+            return True
+        a = self._find_bucket(x)
+        i = bisect_left(a, x)
+        if i != len(a) and a[i] == x: return False
+        a.insert(i, x)
+        self.size += 1
+        if len(a) > len(self.a) * self.REBUILD_RATIO: self._build()
+        return True
+
+    def discard(self, x: int) -> bool:
+        "Remove an element and return True if removed. / O(√N)"
+        if self.size == 0: return False
+        a = self._find_bucket(x)
+        i = bisect_left(a, x)
+        if i == len(a) or a[i] != x: return False
+        a.pop(i)
+        self.size -= 1
+        if len(a) == 0: self._build()
+        return True
+
+    def lt(self, x: int) -> Union[int, None]:
+        "Find the largest element < x, or None if it doesn't exist."
+        for a in reversed(self.a):
+            if a[0] < x: return a[bisect_left(a, x) - 1]
+
+    def le(self, x: int) -> Union[int, None]:
+        "Find the largest element <= x, or None if it doesn't exist."
+        for a in reversed(self.a):
+            if a[0] <= x: return a[bisect_right(a, x) - 1]
+
+    def gt(self, x: int) -> Union[int, None]:
+        "Find the smallest element > x, or None if it doesn't exist."
+        for a in self.a:
+            if a[-1] > x: return a[bisect_right(a, x)]
+
+    def ge(self, x: int) -> Union[int, None]:
+        "Find the smallest element >= x, or None if it doesn't exist."
+        for a in self.a:
+            if a[-1] >= x: return a[bisect_left(a, x)]
+
+    def __getitem__(self, x: int) -> int:
+        "Return the x-th element, or IndexError if it doesn't exist."
+        if x < 0: x += self.size
+        if x < 0: raise IndexError
+        for a in self.a:
+            if x < len(a): return a[x]
+            x -= len(a)
+        raise IndexError
+
+    def index(self, x: int) -> int:
+        "Count the number of elements < x."
+        ans = 0
+        for a in self.a:
+            if a[-1] >= x: return ans + bisect_left(a, x)
+            ans += len(a)
+        return ans
+
+    def index_right(self, x: int) -> int:
         "Count the number of elements <= x."
         ans = 0
         for a in self.a:
