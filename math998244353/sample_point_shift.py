a2dc00429d1469256cc766e19217adcff427abb7
abUma <cryptid.ojin@gmail.com>
2023-12-28 14:25:02 +0900

    行列のrank

--- math998244353/sample_point_shift.py
+++ math998244353/sample_point_shift.py
@@ -8,25 +8,24 @@ def sample_point_shift(y: Vector, t: int, m: int=-1) -> Vector:
     t: shift
     return: f(t + i) for i in range(m)
     '''
-    if m == -1: m == len(y)
-    T = t
+    if m == -1: m = len(y)
     k = len(y) - 1
-    T %= MOD
-    if T <= k:
+    t %= MOD
+    if t <= k:
         ret = [0] * m
         ptr = 0
-        for i in range(T, k + 1):
+        for i in range(t, k + 1):
             ret[ptr] = y[i]
             ptr += 1
             if ptr == m: break
-        if k + 1 < T + m:
+        if k + 1 < t + m:
             suf = sample_point_shift(y, k + 1, m - ptr)
-            for i in range(k + 1, T + m):
+            for i in range(k + 1, t + m):
                 ret[ptr] = suf[i - k - 1]
                 ptr += 1
         return ret
-    if T + m > MOD:
-        pref = sample_point_shift(y, T, MOD - T)
+    if t + m > MOD:
+        pref = sample_point_shift(y, t, MOD - t)
         suf = sample_point_shift(y, 0, m - len(pref))
         return pref + suf
 
@@ -41,14 +40,14 @@ def sample_point_shift(y: Vector, t: int, m: int=-1) -> Vector:
         if (k - i) & 1: d[i] = -d[i]
 
     h = [0] * (m + k)
-    for i in range(m + k): h[i] = modinv(T - k + i, MOD)
+    for i in range(m + k): h[i] = modinv(t - k + i, MOD)
 
     dh = NTT.multiply(d, h)
 
     ret = [0] * m
-    cur = T
-    for i in range(1, k + 1): cur = cur * (T - i) % MOD
+    cur = t
+    for i in range(1, k + 1): cur = cur * (t - i) % MOD
     for i in range(m):
         ret[i] = cur * dh[k + i] % MOD
-        cur = (cur * (T + i + 1) % MOD) * h[i] % MOD
+        cur = (cur * (t + i + 1) % MOD) * h[i] % MOD
     return ret
